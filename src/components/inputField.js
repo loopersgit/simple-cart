616b9e0ca62a3906a3c77d144b7c0bc1e0f8f34d
iqbal <loopersgit>
2018-08-21 18:06:24 +0500

    input

--- src/components/inputField.js
+++ src/components/inputField.js
@@ -8,15 +8,14 @@ class Input extends React.Component {
         this.state = {userName:'',password:''}
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-       this.state = {inputList: [{id: 1,label: 'User Name',user: 'userName', handleChange: this.handleChange('userName')},
-            { id: 2,label: 'Password',user: 'password',handleChange: this.handleChange('password')}]}
+       this.state = {inputList: [{id: 1,label: 'User Name',key: 'userName', handleChange: this.handleChange('userName')},
+            { id: 2,label: 'Password',key: 'password',handleChange: this.handleChange('password')}]}
 
         }
 
     handleChange(key) {
         return function (index) {
             let inputList = this.state.inputList;
-
         inputList[index][key] = this.refs[key].value;
             this.setState({inputList});
         }.bind(this);
@@ -26,22 +25,26 @@ class Input extends React.Component {
     handleSubmit(event) {
 
 
-        var data = {
-            userName: this.state.userName,
-            password: this.state.password,
+         let data = {
+            userName: '',
+            password: '',
         }
+        this.state.inputList.forEach(function (item) {
+            let key = item.key;
+            data[key] = item[key];
+        });
 
-        // const url2 = 'http://localhost:3000/api/user/login';
-        // axios.post(url2,{userCredentials:data})
-        //     .then(resp => {
-        //         let result = resp.data;
-        //         console.log(result.data);
-        //     })
-        //     .catch(error => {
-        //         if (error) {
-        //             console.log(error);
-        //         }
-        //     })
+        const url2 = 'http://localhost:3000/api/user/login';
+        axios.post(url2,{userCredentials:data})
+            .then(resp => {
+                let result = resp.data;
+                console.log(result.data);
+            })
+            .catch(error => {
+                if (error) {
+                    console.log(error);
+                }
+            })
         event.preventDefault();
     }
 
@@ -51,7 +54,7 @@ class Input extends React.Component {
                 <form onSubmit={this.handleSubmit} className="col-lg-12 form-group">
                     {this.state.inputList.map((item, index) => <label key={item.id} className="col-lg-12">
                         {item.label}: <br />
-                        <input key={item.id} type="text" className="form-control" ref={item['user']} onChange={item.handleChange.bind(this,index)} />
+                        <input key={item.id} type="text" className="form-control" ref={item['key']} onChange={item.handleChange.bind(this,index)} />
                     </label>)}
                     <input type="submit" className="btn btn-primary" value="Submit" />
                 </form>
